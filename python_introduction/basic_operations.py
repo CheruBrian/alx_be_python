3dd4ec565832c0a820ab479b72e2fc14ffaef256
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-09-13 16:00:26 +0300

    add basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+
+#perfomn
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+#print
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Mulitplication of {number1} and {number2] is {multiplication}")
