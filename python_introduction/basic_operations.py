d4dbd03c6aabd93a05f4ac7f9c6d28f7acd58c9e
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-09-14 13:09:43 +0300

    Update basic_operations.py
    basic operations

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -9,4 +9,4 @@ multiplication = number1 * number2
 #print
 print(f"Addition of {number1} and {number2} is {addition}")
 print(f"Subtraction of {number1} and {number2} is {subtraction}")
-print(f"Mulitplication of {number1} and {number2] is {multiplication}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
